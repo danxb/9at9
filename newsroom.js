6ef86f14d9f414fdc0f865502653be31f5254175
Dan Brownhill <drb@live.co.uk>
2025-12-06 15:00:10 +0000

    prod browser path

--- newsroom.js
+++ newsroom.js
@@ -97,9 +97,10 @@ ${articleText}
 // --- MAIN RUN ---
 async function run() {
     const browser = await puppeteer.launch({
-  headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/home/dano/.cache/puppeteer/chrome/linux-143.0.7499.40/chrome'
+    });
 
     const feeds = [
         { source: "BBC", category: "News", rss: "https://feeds.bbci.co.uk/news/rss.xml" },
